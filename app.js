4da79994f7205502a8d6155b109eb9fa02860a61
prosper20 <bobsonconnect@gmail.com>
2023-03-04 01:25:47 +0100

    implementing forgot password

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 //const createError = require('http-errors');
 //const morgan = require('morgan');
 
@@ -16,8 +21,32 @@ if (process.env.NODE_ENV === 'development') {
   //app.use(morgan('dev'));
 }
 
-app.use(express.json());
+// Set security HTTP headers
+app.use(helmet());
+
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
+app.use(express.json({ limit: '20kb' }));
 app.use(express.urlencoded({ extended: true }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: ['reviewsQuantity', 'averageRating', 'price'],
+  })
+);
 
 // 3) ROUTES
 app.use('/api/v1/products', productRouter);
