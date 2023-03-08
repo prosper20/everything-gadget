1f81ea0391710c8839e82a74a8d6981afe624e9a
Eteims <8teims@gmail.com>
2023-03-08 23:14:22 +0100

    Added admin feature

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
 //const createError = require('http-errors');
 //const morgan = require('morgan');
+
+const AdminJSExpress = require('@adminjs/express');
+
+const { ADMIN, AdminJS, adminOptions } = require('./admin');
 
 const Problem = require('./utils/problem');
 const errorHandler = require('./contorllers/errorController');
@@ -9,6 +16,8 @@ const userRouter = require('./routes/userRoutes');
 const orderRouter = require('./routes/orderRoutes');
 const blogRouter = require('./routes/blogRoutes');
 
+dotenv.config();
+
 const app = express();
 
 // 1) MIDDLEWARES
@@ -16,21 +25,50 @@ if (process.env.NODE_ENV === 'development') {
   //app.use(morgan('dev'));
 }
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+async function startApp() {
+  await mongoose.connect(process.env.MONGO_URI, {
+    user: process.env.MONGO_USER,
+    pass: process.env.MONGO_PASS,
+    dbname: 'everydb',
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
-// 3) ROUTES
-app.use('/api/v1/products', productRouter);
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/orders', orderRouter);
-app.use('/api/v1/blogs', blogRouter);
+  console.log('Database connection successful');
 
-app.get('*', (req, res, next) => {
-  //throw createError(404, 'PAGE NOT FOUND');
-  next(new Problem(`Can't find ${req.originalUrl} on this server!`, 404));
-});
+  const admin = new AdminJS(adminOptions);
+  const adminRouter = AdminJSExpress.buildAuthenticatedRouter(admin, {
+    authenticate: async (email, password) => {
+      if (ADMIN.password === password && ADMIN.email === email) {
+        return ADMIN;
+      }
+      return null;
+    },
+    cookiePassword: 'somasd1nda0asssjsdhb21uy3g',
+    maxRetries: {
+      count: 3,
+      duration: 120,
+    },
+  });
 
-//error handler
-app.use(errorHandler);
+  app.use(admin.options.rootPath, adminRouter);
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.use('/api/v1/products', productRouter);
+  app.use('/api/v1/users', userRouter);
+  app.use('/api/v1/orders', orderRouter);
+  app.use('/api/v1/blogs', blogRouter);
+
+  app.get('*', (req, res, next) => {
+    //throw createError(404, 'PAGE NOT FOUND');
+    next(new Problem(`Can't find ${req.originalUrl} on this server!`, 404));
+  });
+
+  //error handler
+  app.use(errorHandler);
+}
+
+startApp();
 
 module.exports = app;
